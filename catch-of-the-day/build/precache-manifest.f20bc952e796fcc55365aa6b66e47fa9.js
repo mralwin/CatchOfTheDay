1fa33fb1f773d851d74e43e7487e7c86ff61847d
Owen Thiele <owendthiele@gmail.com>
2019-03-13 19:46:09 +0000

    course compleste push

---
+++ catch-of-the-day/build/precache-manifest.f20bc952e796fcc55365aa6b66e47fa9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "fc0020401956bd4661af",
+    "url": "/static/css/main.003e61ef.chunk.css"
+  },
+  {
+    "revision": "26298f2fe2b8b4a1bfb9",
+    "url": "/static/js/1.26298f2f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "fc0020401956bd4661af",
+    "url": "/static/js/main.fc002040.chunk.js"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "d06c13eb478ad7b945b0c9f3362d47e5",
+    "url": "/index.html"
+  }
+];
---
+++ catch-of-the-day/src/components/Router.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import StorePicker from "./StorePicker"
+import App from './App'
+import NotFound from "./NotFound"
+import React from 'react'
+
+const Router = () => (
+     <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={StorePicker}></Route>
+            <Route path='/store/:storeId' component={App}></Route>
+            <Route component={NotFound}></Route>
+        </Switch>
+    </BrowserRouter>
+)
+
+export default Router
